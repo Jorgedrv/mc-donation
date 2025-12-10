c273c1ba561a827112cc36fb15d9075cb4df868b
Jorge <jorgeddrv@gmail.com>
2025-12-10 11:19:25 -0600

    fix: cors config add allowed origin patterns to avoid cors block from fronted

--- src/main/java/com/tn/donation/mc_donation/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/tn/donation/mc_donation/infrastructure/config/SecurityConfig.java
@@ -76,9 +76,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "https://www.donationflows.com"
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:*",
+                "https://www.donationflows.com",
+                "https://*.donationflows.com"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
